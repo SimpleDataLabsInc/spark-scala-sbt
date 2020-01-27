879d6ec19b58e2dcf6d3e4a7199b938258b5e67b
Maciej Szpakowski <several27@icloud.com>
2020-01-26 17:31:52 -0800

    maven caches

--- shell.sh
+++ shell.sh
@@ -5,5 +5,6 @@ docker run -ti \
 	--name spark-scala-sbt \
 	-p 4040:4040 \
 	-v $(pwd)/playground:/playground \
-        -v $(pwd)/ivy2_cache:/root/.ivy2 \
+	-v $(pwd)/ivy2_cache:/root/.ivy2 \
+	-v $(pwd)/m2_cache:/root/.m2 \
 	${IMAGE}
